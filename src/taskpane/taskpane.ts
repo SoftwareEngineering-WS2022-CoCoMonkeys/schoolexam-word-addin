ab1bf263630f6219dffc06fd420c3871049ebc3d
Tom Papke <tom.papke@outlook.de>
2021-12-08 13:35:04 +0100

    [~] Save created tasks in custom properties

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -10,6 +10,7 @@ Office.onReady((info) => {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
+    document.getElementById("create-task-btn").onclick = createTask;
   }
 });
 
@@ -24,6 +25,30 @@ export async function run() {
 
     // change the paragraph color to blue.
     paragraph.font.color = "blue";
+
+    await context.sync();
+  });
+}
+
+export async function createTask() {
+  return Word.run(async (context) => {
+    // Get the current selection
+    const range = context.document.getSelection();
+
+    // Create a content control
+    const cc = range.insertContentControl();
+
+    // Visually signal content control creation
+    cc.appearance = Word.ContentControlAppearance.boundingBox;
+    cc.title = "DummyTask";
+    cc.tag = "Task";
+
+    // Associate ID with content control
+    context.document.properties.customProperties.add(cc.title + "_ID", 12345);
+    // Set points for task
+    const input = (document.getElementById("points-input") as HTMLInputElement).value;
+    // ... and save persistently in custom properties
+    context.document.properties.customProperties.add(cc.title + "_MaxPoints", input);
 
     await context.sync();
   });
