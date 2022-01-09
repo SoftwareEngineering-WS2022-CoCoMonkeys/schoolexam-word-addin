b4a8059b7378ae24fccb35ccee909c2f3fec1d0f
tompa <tom.papke@outlook.de>
2022-01-09 16:24:15 +0100

    [~] Refactor InstructionList

--- src/taskpane/components/task/NewTaskFromInstructionList.tsx
+++ src/taskpane/components/task/NewTaskFromInstructionList.tsx
@@ -1,56 +1,26 @@
-import * as React from 'react';
-import { TextField, ITextFieldStyles } from '@fluentui/react/lib/TextField';
-import { DetailsList, DetailsListLayoutMode, Selection, IColumn, SelectionMode } from '@fluentui/react/lib/DetailsList';
-
-
-
-const textFieldStyles: Partial<ITextFieldStyles> = { root: { maxWidth: '300px' } };
-
-export interface IInstrutctionListItem {
-  key: number;
-  name: string;
-}
-
-export interface IInstrutctionListProps {
-  items: IInstrutctionListItem[]
-}
+import * as React from "react";
+import { DetailsList, DetailsListLayoutMode, IColumn, SelectionMode } from "@fluentui/react/lib/DetailsList";
 
 export default function InstrutctionList() {
-  var allItems: IInstrutctionListItem[];
-  var columns: IColumn[];
-  
-  allItems = [];
   const instructions = [
     "1. Erstelle die Aufgabe",
     "2. Markiere den (Teil-)Aufgabenbereich",
     "3. Lege die Maximalpunktzahl für die Aufgabe fest",
     "4. Fertig!",
-  ]
-  for (let i = 0; i < instructions.length; i++) {
-    allItems.push({
-      key: i + 1,
-      name:  instructions[i]
-    });
-  }
-  
-  const [items, setItems] = React.useState(allItems);
-  
-  columns = [
-      { key: 'column1', name: 'Vorgehen', fieldName: 'name', minWidth: 300, maxWidth: 400, isResizable: true },
-  ];
+  ].map((instr) => ({ name: instr }));
 
-  console.log(allItems);
-  console.log(items);
+  const columns: IColumn[] = [
+    { key: "column1", name: "Vorgehen", fieldName: "name", minWidth: 300, maxWidth: 400, isResizable: true },
+  ];
 
   return (
     <div id="newTaskInstructionList">
       <DetailsList
-          items={items}
-          columns={columns}
-          selectionMode={SelectionMode.none} 
-          layoutMode={DetailsListLayoutMode.justified}
+        items={instructions}
+        columns={columns}
+        selectionMode={SelectionMode.none}
+        layoutMode={DetailsListLayoutMode.justified}
       />
     </div>
   );
 }
-
